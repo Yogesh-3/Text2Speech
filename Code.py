7920d177e879f42abfc05335bba16f9dbc6712b8
Yogesh R <62652369+Yogesh-3@users.noreply.github.com>
2021-09-07 19:31:22 +0530

    Update Code.py

--- Code.py
+++ Code.py
@@ -1,11 +1,11 @@
-import pyttsx3
-import PyPDF2
-book = open('oop.pdf', 'rb')
-pdfReader = PyPDF2.PdfFileReader(book)
+import pyttsx3     #converts txt to speech
+import PyPDF2      #for reading pdf 
+x = open('GIVE PDF NAME HERE', 'rb')
+pdfReader = PyPDF2.PdfFileReader(x)
 pages = pdfReader.numPages
 
 speaker = pyttsx3.init()
-for num in range(7, pages):
+for num in range(1, pages):
     page = pdfReader.getPage(num)
     text = page.extractText()
     speaker.say(text)
